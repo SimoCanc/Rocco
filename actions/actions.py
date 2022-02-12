42f1fd72e252c5262e2ff0f60e246ea5004ef1b7
Simone <simocance@gmail.com>
2022-02-12 14:44:10 +0100

    prima versione funzionante - corretta sintassi di dominio ed azioni

--- actions/actions.py
+++ actions/actions.py
@@ -4,57 +4,53 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, AllSlotsReset
 
-#TODO: mettere alternativa ELSE in caso di nessun match nell IF
 #action_call_XXX -> Settano le slot slots plumber, electrician, painter 
 class ActionCallPlumber(Action):
-  def name(self) -> Text
-      return "action_call_electrician"  
+  def name(self) -> Text:
+      return "action_call_plumber" 
   def run(self, 
           dispatcher: CollectingDispatcher,
           tracker: Tracker,
           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-      #TODO: Provare a mettere la variabile selected_intent come globale
-      selected_intent = Tracker.get_intent_of_latest_message()[-3:]
+      selected_intent = Tracker.get_intent_of_latest_message(tracker)[-3:]
       if selected_intent == "ENG":
         SlotSet("slot_plumber", "plumber")
-      elif selected_intent == "ITA"
+      elif selected_intent == "ITA":
         SlotSet("slot_plumber", "idraulico") 
       return []   
 
 class ActionCallElectrician(Action):
-  def name(self) -> Text
+  def name(self) -> Text:
       return "action_call_electrician"  
   def run(self, 
           dispatcher: CollectingDispatcher,
           tracker: Tracker,
           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-      #TODO: Provare a mettere la variabile selected_intent come globale
-      selected_intent = Tracker.get_intent_of_latest_message()[-3:]
+      selected_intent = Tracker.get_intent_of_latest_message(tracker)[-3:]
       if selected_intent == "ENG":
         SlotSet("slot_plumber", "electrician")
-      elif selected_intent == "ITA"
+      elif selected_intent == "ITA":
         SlotSet("slot_plumber", "elettricista") 
       return []   
     
 
 class ActionCallPainter(Action):
-  def name(self) -> Text
+  def name(self) -> Text:
       return "action_call_painter"  
   def run(self, 
           dispatcher: CollectingDispatcher,
           tracker: Tracker,
           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-      #TODO: Provare a mettere la variabile selected_intent come globale
-      selected_intent = Tracker.get_intent_of_latest_message()[-3:]
+      selected_intent = Tracker.get_intent_of_latest_message(tracker)[-3:]
       if selected_intent == "ENG":
         SlotSet("slot_plumber", "painter")
-      elif selected_intent == "ITA"
+      elif selected_intent == "ITA":
         SlotSet("slot_plumber", "imbianchino") 
       return []   
 
 #Resetta tutte le slots
 class ResetSlots(Action):
-  def name(self) -> Text
+  def name(self) -> Text:
       return "action_reset_slots"  
   def run(self, 
           dispatcher: CollectingDispatcher,
